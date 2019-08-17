7572d0c06c2e7e076cb5fba7c3e792434c9a64e4
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-17 19:36:56 +0900

    Study, DispatchQueue에 대해서, 스레드 동작방식 및 사용법

--- AsyncStudy.playground/Contents.swift
+++ AsyncStudy.playground/Contents.swift
@@ -2,6 +2,8 @@ import UIKit
 
 var str = "Hello, playground"
 
+// MARK: -
+
 // Concurrency를 공부 하기 전? 5가지 FAQ
 // 1) What is concurrency? 무엇이 동시성인가?
 // DispatchQueue, OperationQueue
@@ -19,9 +21,28 @@ var str = "Hello, playground"
 // 5) Where do tasks run?
 // 어디서 동작이 실행되는가??
 
+// MARK: -
+
 // * Concurrency 프로그래밍은 위험성을 담고 있다. 실제론 동시적 순서로 실행되는 것이 아니기 때문이다.
 // 1) Race condition 상의 문제가 생길 수 있다. => 의도치 않은 스레드에서 연산이 끝나버릴 수 있음.
 // serialQueue의 사용,
 // 2) priority Inversion
 // low -> medium -> high Priority Task -> medium이 종료, low가 재개하지만 high task는 실행되는 상태...
 // Deadrock, 교착상태에 빠지며 메모리 누수가 발생할 수 있다.
+
+// Concurrency 와 Synchronous는 같은가?? "NO"
+// Serial vs Concurrent? => 한개의 스레드냐 다수의 스레드냐??
+// DispatchQueue.globlal() // .default qos(Quality Of Service Level, 서비스 레벨의 지정 가능)
+
+// DispatchQueue의 사용 방법들 및 스레드 동작 방식
+let userQueue = DispatchQueue.global(qos: .userInitiated)
+// Quality Of Service 우선순위를 지정한 Global Concurrent Queue -> 서브 스레드에서 동작
+
+let defaultQueue = DispatchQueue.global()
+// Global 스레드를 사용한 Concurrent Queue -> 서브 스레드에서 동작
+
+let mainQueue = DispatchQueue.main // FIFO
+// ✭ 메인스레드에서 동작하는 Serial Queue -> 메인 스레드에서 동작
+
+let workerQueue = DispatchQueue(label: "com.raywenderlich.worker")
+// 시리얼 큐, Sync -> Main Thread 에서 동작 Async -> Sub Thread 에서 동작
